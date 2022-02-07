12d8615cef51a1f690ff10b78f8e6d7f1726be85
haihung <hungnh12062000@gmail.com>
2022-02-07 16:06:22 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+//links
+
+const links = document.querySelectorAll(".link");
+// console.log(links);
+
+links.forEach((link) => {
+  link.onclick = function () {
+    document.querySelector(".link.active").classList.remove("active");
+    link.classList.add("active");
+  };
+});
+
+//create dynamic project card
+
+const projectContainer = document.querySelector(".project-container");
+// console.log(projectContainer);
+
+projects.forEach((project) => {
+  projectContainer.innerHTML += `
+    <div class="project-card" data-tags="#all, ${project.tags}">
+      <img src="./assets/img/${project.image}" alt="">
+      <div class="project-content">
+        <div class="project-name">${project.name}</div>
+        <span class="tags">${project.tags}</span>
+      </div>
+    </div>
+  `;
+});
+
+//filter
+const filters = document.querySelectorAll(".filter-btn");
+
+filters.forEach((filterBtn) => {
+  filterBtn.addEventListener("click", () => {
+    let id = filterBtn.getAttribute("id");
+    let projectCards = document.querySelectorAll(".project-card");
+    projectCards.forEach((card) => {
+      if (card.getAttribute("data-tags").includes(id)) {
+        card.classList.remove("hide");
+      } else {
+        card.classList.add("hide");
+      }
+
+      filters.forEach((btn) => btn.classList.remove("active"));
+      filterBtn.classList.add("active");
+    });
+  });
+});
---
+++ assets/js/project.js
@@ -0,0 +1,47 @@
+let projects = [
+  {
+    name: "Travel",
+    tags: "#HTML, #CSS, #Web-application",
+    image: "project_1.png",
+  },
+  {
+    name: "Selftaught",
+    tags: "#HTML, #SCSS, #Web-application",
+    image: "project_2.png",
+  },
+  {
+    name: "Canifa fashion",
+    tags: "#HTML, #CSS, #JS, #E-commerce",
+    image: "project_3.png",
+  },
+  {
+    name: "Playground",
+    tags: "#HTML, #CSS, #SCSS, #Web-application",
+    image: "project_4.png",
+  },
+  {
+    name: "UITruyen",
+    tags: "#Figma",
+    image: "project_5.png",
+  },
+  {
+    name: "Sign in",
+    tags: "#Figma",
+    image: "project_6.png",
+  },
+  {
+    name: "Shoe",
+    tags: "#Figma",
+    image: "project_7.png",
+  },
+  {
+    name: "Apple watch",
+    tags: "#Figma",
+    image: "project_8.png",
+  },
+  {
+    name: "Opipi",
+    tags: "#HTML, #CSS, #JS, #Web-application",
+    image: "project_9.png",
+  },
+];
