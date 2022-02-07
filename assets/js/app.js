646a985195d7dc78b94938f4d0cc1bfdd8de4ab0
haihung <hungnh12062000@gmail.com>
2022-02-07 20:44:39 +0700

    responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,3 +46,12 @@ filters.forEach((filterBtn) => {
     });
   });
 });
+
+//responsive toggle button
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', ()=>{
+  toggleBtn.classList.toggle('active');
+  linkContainer.classList.toggle('show');
+})
